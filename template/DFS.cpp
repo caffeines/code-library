7b2c7dec9e8e898cb8c7e427600c61fd972362bb
caffeines <me.caffeines@gmail.com>
2017-12-18 20:50:23 +0600

    18.12.2017

---
+++ template/DFS.cpp
@@ -0,0 +1,14 @@
+#define N 1000007
+vector <int> graph[N]; //array of vector to store node
+bool visited [N]; //marker for nodes
+void dfs(int curr)
+{
+    visited[curr] = true; //marking current node as visited
+    for(int i=0;i<graph[curr].size();i++) //looping through all the adjacent nodes
+    {
+        if(!visited[graph[curr][i]]) //checking if we found any adjacent node is not visited
+        {
+            dfs(graph[curr][i]); //visiting the not visited node
+        }
+    }
+}
