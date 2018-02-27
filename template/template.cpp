5f0579c24120b8b617112b34cbe2c972eec3ceb4
caffeines <me.caffeines@gmail.com>
2018-02-27 19:21:50 +0600

    27.02.2018

---
+++ template/template.cpp
@@ -0,0 +1,35 @@
+/*===============================================
+    Username    : Caffeines
+    Name        : Abu Sadat Md. Sayem
+    about me    : caffeines.github.io
+    University  : North South University (NSU)
+=================================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pf printf
+#define ll long long
+#define INF 0xfffffff
+#define pl printf("\n")
+#define pb(x) push_back(x)
+#define sf(n) scanf("%d",&n)
+#define slf(n) scanf("%lld",&n)
+#define mem(a) memset(a,0,sizeof(a))
+#define sff(n,m) scanf("%d %d",&n,&m)
+#define slff(n,m) scanf("%lld %lld",&n,&m)
+
+/*=========================================================
+    stop doubting yourself, work hard and make it happen.
+==========================================================*/
+
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    int x;
+
+
+    return 0;
+}
