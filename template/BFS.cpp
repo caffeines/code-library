2a9fcd637b85ef7df7dffa6377d77207d7f3a81d
caffeines <me.caffeines@gmail.com>
2018-03-02 12:56:51 +0600

    02.03.2018

---
+++ template/BFS.cpp
@@ -0,0 +1,25 @@
+#define N 10001
+vector <int> graph[N];
+bool visit[N];
+int dist [N];
+
+void bfs(int start)
+{
+    queue <int> q;
+    visit[start] = true;
+    dist[start] = 0;
+    q.push(start); //insert in queue
+    while(!q.empty())
+    {
+        int u = q.front(); q.pop();
+        for(int i = 0;i<graph[u].size();i++) // check all adjacent node
+        {
+            int v = graph[u][i];
+            if(visit[v])
+                continue;
+            visit[v] = true;
+            dist[v] = dist[u]+1;
+            q.push(v);
+        }
+    }
+}
