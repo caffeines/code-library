9be48cbfd682ae5864fce0ac5079601a67af5f5b
caffeines <me.caffeines@gmail.com>
2018-04-19 19:27:19 +0600

    cool

--- Programming-Contest/Graph Theory/dijkstra.cpp
+++ Programming-Contest/Graph Theory/dijkstra.cpp
@@ -1,31 +1,7 @@
-/*===============================================
-    Username    : Caffeines
-    Name        : Abu Sadat Md. Sayem
-    about me    : caffeines.github.io
-    University  : North South University (NSU)
-=================================================*/
 
-#include <bits/stdc++.h>
-using namespace std;
-#define sc scanf
-#define N 100010
-#define pf printf
-#define ll long long
-#define INF 0xfffffff
-#define pl printf("\n")
-#define pb(x) push_back(x)
-#define sf(n) scanf("%d",&n)
-#define slf(n) scanf("%lld",&n)
-#define mem(a,b) memset(a,b,sizeof(a))
-#define sff(n,m) scanf("%d %d",&n,&m)
-#define slff(n,m) scanf("%lld %lld",&n,&m)
-
-/*=========================================================
-    stop doubting yourself, work hard and make it happen.
-==========================================================*/
 struct Node{
     int id;
-    int cost;
+    ll cost;
     Node(){};
     Node(int id,int cost)
     {
@@ -35,72 +11,47 @@ struct Node{
 
     bool operator < (const Node &node) const
     {
-        return cost < node.cost;
+        return cost > node.cost;
     }
 
-}curr,next;
+}curr,nxt;
 
-vector <pair<int,int> > graph[N];
-vector <Node> vec;
-int dist[N];
+int path[Size];
+priority_queue <Node> pq;
+vector <pii> graph[Size];
+ll dist[Size];
+
 void dijkstra(int source)
 {
-    dist[source] = 0;
+    FOR(i,0,Size) dist[i] = inf;
     curr.id = source;
     curr.cost = 0;
-    priority_queue < Node> pq;
+    dist[source] = 0;
+
     pq.push(curr);
     while(!pq.empty())
     {
         curr = pq.top();
         pq.pop();
-        if(curr.cost != dist[curr.id])
-            continue;
-        for(int i = 0;i <graph[curr.id].size();i++)
+
+        for(int i=0;i < graph[curr.id].size();i++)
         {
-            next.id = graph[curr.id][i].first;
-            next.cost = graph[curr.id][i].second + dist[curr.id];
-            if(dist[next.id] > next.cost)
+            nxt.id = graph[curr.id][i].first;
+            nxt.cost = graph[curr.id][i].second + dist[curr.id];
+
+            if(dist[nxt.id] > nxt.cost || dist[nxt.id] == inf)
             {
-                dist[next.id] = next.cost;
-                pq.push(next);
+                dist[nxt.id] = nxt.cost;
+                path[nxt.id] = curr.id ;
+                pq.push(nxt);
             }
         }
     }
 }
 
-
-void Clear()
+void printPath(int en )
 {
-    for(int i = 0;i<N;i++)
-        graph[i].clear();
-    mem(dist,1);
-}
-
-int main()
-{
-    Clear();
-    freopen("input.txt","r",stdin);
-    //freopen("output.txt","w",stdout);
-    int n,e,u,v,w,temp;
-    sff(n,e);
-    temp = e;
-    while(e--)
-    {
-        sff(u,v);sf(w);
-
-        Node U(u,w),V(v,w);
-        graph[u].pb(make_pair(v,w));
-        graph[v].pb(make_pair(v,w));
-        vec.pb(U);
-        vec.pb(V);
-    }
-    dijkstra(1);
-
-    for(int i = 1;i<temp;i++)
-    {
-        cout<<i<<"  "<<dist[i]<<endl;
-    }
-
-    return 0;
+    if( en ==0) return;
+    printPath(path[en]) ;
+    printf("%d ",en) ;
 }
--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # Code - library
 
-**Data Structure**
-This folder contains vasious kind of data structure impementation by using C++.
-
 **Impementation**
 All the codes that I have written/collected/organized to solve various algorithmic problems.
