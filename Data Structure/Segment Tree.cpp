0aec6a96f32c3674826e039057502307d13e322f
Abu Sadat Md. Sayem <me.caffeines@gmail.com>
2018-11-22 20:03:10 +0600

    Update Segment Tree.cpp

--- Data Structure/Segment Tree.cpp
+++ Data Structure/Segment Tree.cpp
@@ -1,50 +1,50 @@
 #define N 100001
 int arr[N];
-int tree[N*3];
-void init(int curr,int start,int end)
-{
-    if(end == start)
-    {
+int tree[N * 3];
+
+// init(1,1,N)
+void init(int curr, int start, int end) { //
+    if (end == start) {
         tree[curr] = arr[end];
         return;
     }
-    int left = curr*2;
-    int right = (curr*2)+1;
-    int mid = (start+end)/2;
-    init(left,start,mid);
-    init(right,mid+1,end);
+    int left = curr * 2;
+    int right = (curr * 2) + 1;
+    int mid = (start + end) / 2;
+    init(left, start, mid);
+    init(right, mid + 1, end);
     tree[curr] = tree[left] + tree[right];
 }
 
-void update(int curr,int start, int end,int pos,int value)
-{
-    if(start==pos && end==pos)
-    {
+//update(1, 1, N,pos,value)
+void update(int curr, int start, int end, int pos, int value) {
+    if (start == pos && end == pos) {
         tree[curr] = value;
         return;
     }
-    if(start>end || start>pos || end<curr)
+    if (start > end || start > pos || end < curr)
         return;
-    int left = curr*2;
-    int right = (curr*2)+1;
-    int mid = (start+end)/2;
+    int left = curr * 2;
+    int right = (curr * 2) + 1;
+    int mid = (start + end) / 2;
 
-    update(left,start,mid,pos,value);
-    update(right,mid+1,end,pos,value);
-    tree[curr] = tree[left]+tree[right];
+    update(left, start, mid, pos, value);
+    update(right, mid + 1, end, pos, value);
+    tree[curr] = tree[left] + tree[right];
 }
-int query(int curr,int start,int end,int first,int last)
-{
-    if(start>=first && end<=last)
+
+// query(1, 1, N, search_position_first,rsearch_position_last)
+int query(int curr, int start, int end, int first, int last) {
+    if (start >= first && end <= last)
         return tree[curr];
-    if(first>end || last<start)
+    if (first > end || last < start)
         return 0;
 
-    int left = curr*2;
-    int right = (curr*2)+1;
-    int mid = (start+end)/2;
+    int left = curr * 2;
+    int right = (curr * 2) + 1;
+    int mid = (start + end) / 2;
 
-    int q1 = query(left,start,mid,first,last);
-    int q2 = query(right,mid+1,end,first,last);
-    return q1+q2;
+    int q1 = query(left, start, mid, first, last);
+    int q2 = query(right, mid + 1, end, first, last);
+    return (q1 + q2);
 }
